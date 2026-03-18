d8a92b8a65fcd8efc0dbd70f8b61f9bccf509f1a
nathan <nathan.ta2001@gmail.com>
2026-03-17 21:25:39 -0300

    chore: reestruturando raiz do projeto

---
+++ src/main/java/org/nathan/primeiracasabackend/PrimeiraCasaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nathan.primeiracasabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraCasaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraCasaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nathan/primeiracasabackend/PrimeiraCasaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nathan.primeiracasabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraCasaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
