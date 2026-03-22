a24a263a67e58c7bf9f1f4066ec841580c761462
nathan <nathan.ta2001@gmail.com>
2026-03-22 18:30:18 -0300

    bug: conteudo da classe removido

--- src/test/java/org/nathan/primeiracasabackend/PrimeiraCasaBackendApplicationTests.java
+++ src/test/java/org/nathan/primeiracasabackend/PrimeiraCasaBackendApplicationTests.java
@@ -1,13 +1,11 @@
 package org.nathan.primeiracasabackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PrimeiraCasaBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    void contextLoads() {
+    }
 
 }
